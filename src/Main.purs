89baeab3d2e1d0f231df108f51e47e901ae3110e
Charles Taylor <charlestaylor95@gmail.com>
2023-09-17 15:51:45 -0400

    set incsearch

--- src/Main.purs
+++ src/Main.purs
@@ -5,6 +5,9 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+foreign import setNeovimOption :: forall a. String -> a -> Effect Unit
+
 main :: Effect Unit
 main = do
+  setNeovimOption "incsearch" true
   log "🍝"
